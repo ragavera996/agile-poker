ba06b954e0c9b036f8b14bec5e6aa78cb7225ed6
Ragavendran Ravi <ragavshankar996@gmail.com>
2025-01-02 20:17:57 -0500

    Inital commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const roomsRouter = require('./routes/rooms');
+const handleSocket = require('./socket/handlers');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/rooms', roomsRouter);
+
+handleSocket(io);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ src/app/services/room.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RoomState } from '../models/room.model';
+
+@Injectable({ providedIn: 'root' })
+export class RoomService {
+  private apiUrl = 'http://localhost:3000/api/rooms';
+
+  constructor(private http: HttpClient) {}
+
+  createRoom(name: string) {
+    return this.http.post<RoomState>(this.apiUrl, { name });
+  }
+
+  getRoom(id: string) {
+    return this.http.get<RoomState>(`${this.apiUrl}/${id}`);
+  }
+}
