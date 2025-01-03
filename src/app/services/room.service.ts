e55f5e6b7ff3e1a41249b4315f89e4cd2a16e54e
Ragavendran Ravi <ragavshankar996@gmail.com>
2025-01-02 20:59:13 -0500

    Add domains

--- src/app/services/room.service.ts
+++ src/app/services/room.service.ts
@@ -4,7 +4,7 @@ import { RoomState } from '../models/room.model';
 
 @Injectable({ providedIn: 'root' })
 export class RoomService {
-  private apiUrl = 'https://agile-poker-server.vercel.app:3000/api/rooms';
+  private apiUrl = 'https://agile-poker-server.vercel.app/api/rooms';
 
   constructor(private http: HttpClient) {}
 
--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -9,7 +9,7 @@ export class SocketService {
   private roomState = new BehaviorSubject<RoomState | null>(null);
 
   constructor() {
-    this.socket = io('https://agile-poker-server.vercel.app:3000/');
+    this.socket = io('https://agile-poker-server.vercel.app/');
     this.socket.on('room-state', (state: RoomState) => {
       this.roomState.next(state);
     });
